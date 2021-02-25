71037256151904770e4ee2fc2b3475a2e27c4a05
Dan Nguyen <nghuudan@gmail.com>
2021-02-24 19:17:04 -0500

    Add code, build, and tests

---
+++ src/index.spec.ts
@@ -0,0 +1,43 @@
+import retryPromise from '.';
+
+describe('retryPromise', () => {
+  const errorMock = new Error('error');
+  const resolveMock = () => Promise.resolve('test');
+  const rejectMock = () => Promise.reject(errorMock);
+
+  it('should return a promise', () => {
+    expect(retryPromise(resolveMock)).toBeInstanceOf(Promise);
+  });
+
+  it('should resolve with the value from the promise function', async () => {
+    expect(await retryPromise(resolveMock)).toBe('test');
+  });
+
+  it('should reject with the retry promise error message', async () => {
+    try {
+      await retryPromise(rejectMock);
+    } catch (error) {
+      expect(error.message).toBe('Maximum retries reached');
+    }
+  });
+
+  it('should reject with the original error message in the error', async () => {
+    try {
+      await retryPromise(rejectMock);
+    } catch (error) {
+      expect(error.originalErrorMessage).toBe(errorMock.message);
+    }
+  });
+
+  it('should try for the number of retries in options', async () => {
+    let totalCount = 0;
+    try {
+      await retryPromise(() => {
+        totalCount += 1;
+        return Promise.reject(errorMock);
+      }, { delay: 10, multiplier: 2, retries: 5 });
+    } catch {
+      expect(totalCount).toBe(6);
+    }
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+export class RetryPromiseError extends Error {
+  /** Message from the original error */
+  originalErrorMessage: string;
+
+  /**
+   * Creates a RetryPromiseError including the original error message
+   * @param error original error to include
+   */
+  constructor(error: Error) {
+    super('Maximum retries reached');
+    this.originalErrorMessage = error.message;
+  }
+}
+
+export type RetryPromiseFunction<T = void> = () => Promise<T>;
+
+export interface RetryPromiseOptions {
+  /** Delay between each retry in milliseconds */
+  delay?: number;
+  /** Multiplier for the delay after each retry */
+  multiplier?: number;
+  /** Number of retries if the promise function is rejected */
+  retries?: number;
+}
+
+/**
+ * Retries a function returning a promise with a delay between each retry
+ * @param promiseFn function returning a promise
+ * @param options delay in milliseconds, multiplier, and retries
+ * @default options { delay: 1000, multiplier: 1, retries: 3 }
+ */
+const retryPromise = <T = void>(
+  promiseFn: RetryPromiseFunction<T>,
+  options?: RetryPromiseOptions,
+) => {
+  const {
+    delay,
+    multiplier,
+    retries,
+  }: RetryPromiseOptions = {
+    delay: 1000,
+    multiplier: 1,
+    retries: 3,
+    ...options,
+  };
+  return new Promise<T>((resolve, reject) => {
+    promiseFn().then(resolve).catch((error) => {
+      if (retries > 0) {
+        retryPromise(promiseFn, {
+          delay: delay * multiplier,
+          retries: retries - 1,
+          multiplier,
+        }).catch(reject);
+      } else {
+        const retryError = new RetryPromiseError(error);
+        reject(retryError);
+      }
+    });
+  });
+};
+
+export default retryPromise;
